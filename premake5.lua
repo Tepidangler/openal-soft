dfcb33fdcad1f5af8edac2bb1377c63cac0a17ec
Tepid Angler <32462088+Tepidangler@users.noreply.github.com>
2024-08-06 17:46:54 -0400

    Add files via upload

---
+++ premake5.lua
@@ -0,0 +1,42 @@
+project "OpenAL"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"**.h",
+		"**.cpp",
+		
+
+	}
+
+	defines{
+		
+	}
+
+	includedirs
+	{
+		"include/AL"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "on"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "off"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
